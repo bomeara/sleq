7a6596d3daa9d5a05a43698df74a38c6a18b43b4
Brian O'Meara <omeara.brian@gmail.com>
2015-10-01 21:07:39 -0400

    skeleton plus help, using devtools and roxygen2

---
+++ R/sleq.R
@@ -0,0 +1,8 @@
+#' Utilities for dealing with sequence data in R
+#'
+#' sleq was inspired by \pkg{seqinr} and phyutility: code
+#' that makes dealing with sequences for phylogenetics easir
+#'
+#' @docType package
+#' @name sleq
+NULL
