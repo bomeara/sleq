132c23246f503ed23d428d7f7f675aea2f055d64
Brian O'Meara <omeara.brian@gmail.com>
2015-10-01 22:25:30 -0400

    plotting now has colors. Ugly colors.

--- R/CreateBirdsEyeView.R
+++ R/CreateBirdsEyeView.R
@@ -1,6 +1,6 @@
 #' Create a birds' eye view of the data
 #' @param data A character matrix
-#' @param colors A vector of colors
+#' @param colors A vector of colors, with colnames matching states
 #' @return TRUE if the plotting worked
 #' @examples
 #' \dontrun{
@@ -9,7 +9,17 @@
 #' CreateBirdsEyeView(test.sequences)
 #' }
 CreateBirdsEyeView <- function(data, colors=NULL) {
-  plot(x=c(1,dim(data)[2]), y=c(1,dim(data)[1]), type="n", xlab="", ylab="", yaxt="n", bty="n")
-  
+  plot(x=c(.5,.5+dim(data)[2]), y=c(-.5,-.5-dim(data)[1]), type="n", xlab="", ylab="", yaxt="n", bty="n")
+  text(x=rep(0,dim(data)[1]), y=-sequence(dim(data)[1]), labels=rownames(data), adj = c(1, NA), cex=0.5)
+
+  if(is.null(colors)) {
+  	colors <- rainbow(n=length(unique(data))) #primitive way of doing now. Later, do gaps, N, differently
+  	names(colors) <- unique(data)
+  }
+  for (taxon in sequence(dim(data)[1])) {
+    for (character in sequence(dim(data)[2])) {
+    	rect(xleft=-.5+character, ybottom=-.45-taxon, xright=0.5+character, ytop=.45-taxon, col=colors[data[taxon,character]], border=NA)
+    }
+  }
   return(TRUE)
 }
