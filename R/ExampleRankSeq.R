d77858e2fc73992939ab4feee2b89c19361af5d7
Brian OMeara <omeara.brian@gmail.com>
2015-10-13 10:49:44 -0400

    commented out 'Example' files, but left them there, to put into roxygen format later

---
+++ R/ExampleRankSeq.R
@@ -0,0 +1,54 @@
+# Other example Latrodectus hesperus 
+#accession.number<-c("KP656931","DQ127326","KP656707","KC414081","KC414080")
+#library(ape)
+#seqs.onespecies<- read.GenBank(access.nb=accession.number, species.names=TRUE,gene.names=TRUE, as.character=TRUE)
+
+#seqs.onespeciesv2<- read.GenBank(access.nb=accession.number, species.names=TRUE,gene.names=TRUE, as.character=FALSE)
+
+#no.sites<-500
+#no.seqs<-5
+
+#sequences.example2<-rbind(seqs.onespecies[[1]][1:500], seqs.onespecies[[2]][1:500], seqs.onespecies[[3]][1:500], seqs.onespecies[[4]][1:500],seqs.onespecies[[5]][1:500] )
+
+#summary.example2<-SingleTaxonCompare(sequences.example2)#
+
+#print(summary.example2[1:10])
+#3 474
+#
+#percentage.rankmatrix<-matrix(rep(0,no.sites*4), nrow=4)
+#nucleotide.rankmatrix<-matrix(rep("l",no.sites*4),nrow=4)
+#rank.qualityseq<-rep(0,no.seqs)
+
+#for(i in 1:10){
+#aux<-sort(summary.example2[[i]][1:4,2],decreasing=TRUE)
+#percentage.rankmatrix[,i]<-aux
+#nucleotide.rankmatrix[,i]<-names(aux)
+
+#aux1<-match(percentage.rankmatrix[,i],percentage.rankmatrix[,i])
+#aux2<-which(aux1==1)
+#target.nucleotide<-nucleotide.rankmatrix[aux2,i]
+#case.number<-length(target.nucleotide)
+#if(case.number==1){
+#		penalty.value<-1
+#}else{
+#	if(case.number==2){
+#		penalty.value<-0.5
+#	}else{
+#		if(case.number==3){
+#			penalty.value=0.3
+#		}else{ 
+#			penalty.value=0
+#		}
+#	}	
+#}
+
+#target.seqsite<-which(sequences.example2[,i]%in%target.nucleotide)
+#rank.qualityseq[target.seqsite]<-rank.qualityseq[target.seqsite]+penalty.value
+#}
+
+
+
+#a=percentage.rankmatrix[,3]
+#match(a,a)
+#summary.example2[1:5]
+
---
+++ R/ExampleSingleTaxonCompare.R
@@ -0,0 +1,21 @@
+#Testing SingleTaxonCompare function
+
+#Example with woodmouse data
+
+
+#library(ape) 
+#data(woodmouse)
+#mice.raw<-as.character(woodmouse)
+#mice.seqalignment<-seqalignment(mice.raw)
+
+#sequence.example1<- mice.seqalignment$sequences[,c(12,14,30,960,963)]
+#12,14, 30 960, 963
+
+#summary.sites<-SingleTaxonCompare(sequences=sequence.example1)
+#print(summary.sites)
+
+# Other example Latrodectus hesperus 
+#accession.number<-c("KP656931","DQ127326")
+#library(ape)
+#seqs.onespecies<- read.GenBank(access.nb=accession.number, species.names=TRUE,gene.names=TRUE, as.character=TRUE)
+#sequences.example2<-rbind(seqs.onespecies[[1]][1:500], seqs.onespecies[[2]][1:500])
